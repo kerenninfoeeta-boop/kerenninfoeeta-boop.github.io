b94ca000a297d99b4ca293d4e520ce0cd495c960
Kerenn Happuque <kerenninfoeeta@gmail.com>
2026-07-09 20:43:03 -0300

    Create supabase.js

---
+++ JS/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://hzmylgleillvkvdozmfy.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_Ld4Z6SDdkDp9FB-O1uMzsA_829oc34P";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
