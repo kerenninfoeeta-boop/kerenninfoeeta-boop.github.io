8eaa3d9b14ff70d6e9605d7a98886663681a80e8
Kerenn Happuque <kerenninfoeeta@gmail.com>
2026-07-10 10:44:09 -0300

    Teste do botão de agendamento

--- JS/script.js
+++ JS/script.js
@@ -1,60 +1,23 @@
-
 console.log("Script carregado");
 
 document.addEventListener("DOMContentLoaded", function () {
 
+    console.log("Página carregada");
+
     const formulario = document.getElementById("formAgendamento");
 
-    if (!formulario) return;
+    console.log(formulario);
+
+    if (!formulario) {
+        console.log("Formulário não encontrado");
+        return;
+    }
 
     formulario.addEventListener("submit", async function (event) {
 
         event.preventDefault();
 
-        const nome = document.getElementById("nome").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const telefone = document.getElementById("telefone").value.trim();
-        const especialidade = document.getElementById("especialidade").value;
-        const data = document.getElementById("data").value;
-        const horario = document.getElementById("horario").value;
-        const observacao = document.getElementById("observacao").value.trim();
-
-        if (
-            !nome ||
-            !email ||
-            !telefone ||
-            !especialidade ||
-            !data ||
-            !horario
-        ) {
-            alert("Preencha todos os campos obrigatórios.");
-            return;
-        }
-
-        const { error } = await supabaseClient
-    .from("consultas")
-            .insert([
-                {
-                    nome,
-                    email,
-                    telefone,
-                    especialidade,
-                    data,
-                    horario,
-                    observacao,
-                    status: "Agendada"
-                }
-            ]);
-
-        if (error) {
-            console.error(error);
-            alert("Erro ao salvar a consulta.");
-            return;
-        }
-
-        alert("Consulta agendada com sucesso!");
-
-        formulario.reset();
+        alert("O clique chegou aqui!");
 
     });
 
